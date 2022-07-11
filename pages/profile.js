02c746403ffe7d8d9bf2c6bdc3889a3db2bc8d99
tsuguri <tsuguri4429@gmail.com>
2022-07-11 17:18:41 +0900

    fix profile

--- pages/profile.js
+++ pages/profile.js
@@ -29,22 +29,70 @@ export default function Profile() {
         </div>
         <div className="pt-10 pb-8 text-gray-900 dark:text-gray-100 max-w-none xl:col-span-2">
           <div className="flex flex-col border-2 border-gray-500 rounded border-opacity-60">
-            <h2 className="px-4 py-4 text-xl font-bold tracking-normal text-gray-900 dark:text-gray-100 md:text-2xl">
+            <h2 className="px-4 pt-6 pb-1 text-xl font-bold tracking-normal text-gray-900 dark:text-gray-100 md:text-2xl">
               自己紹介
             </h2>
-            <p className="px-4 py-4 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
-              情報分野学科の大学生です。
+            <p className="px-4 pt-6 pb-1 text-lg font-semibold leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              所属
             </p>
-            <p className="px-4 py-4 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
-              Webサイト、ネイティブアプリなどの様々なプロダクトを開発しています。このブログの開発で使用した技術はJavaScript、Next.js、TailwindCSS、microCMSです。
+            <p className="px-4 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              関西学院大学理工学部情報科学科大崎研究室、on株式会社（エンジニアインターンシップ生）
             </p>
-            <p className="px-4 py-4 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
-              このWebサイトでは「開発したプロダクト」、「情報を共有するブログ」を掲載します。
+            <p className="px-4 pt-6 pb-1 text-lg font-semibold leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              好きなこと
+            </p>
+            <p className="px-4 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              プログラミング、ものづくり、麻雀、スポーツ観戦
+            </p>
+            <p className="px-4 pt-6 pb-1 text-lg font-semibold leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              嫌いなこと
+            </p>
+            <p className="px-4 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              ネガティブ思考、きのこ、前例が少ないエラー
+            </p>
+            <p className="px-4 pt-6 pb-1 text-lg font-semibold leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              経歴
+            </p>
+            <p className="px-4 py-3 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              2000年：広島県呉市で誕生（広島での記憶はほどんどない）
+            </p>
+            <p className="px-4 py-3 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              2004年：兵庫県西宮市に引っ越し
+            </p>
+            <p className="px-4 py-3 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              2013年：中学生になる。テニス、ゲーム、ものづくりに没頭
+            </p>
+            <p className="px-4 py-3 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              2016年：高校生になる。テニスに高校生活を捧げる。
+            </p>
+            <p className="px-4 py-3 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              2019年：大学生になる。プログラミングに初めて触れる。プログラミングでプロダクトを作りたいと思う。
+            </p>
+            <p className="px-4 py-3 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              2021年：プログラミングで初めてプロダクト(TsuguBlog)を作成。
+            </p>
+            <p className="px-4 py-3 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              2022年：2つ目のプロダクトiosアプリmanasubを作成、リリース(収益: 500円)。on株式会社にインターン生としてjoin。大学では大崎研究室にjoin
+            </p>
+            <p className="px-4 py-3 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              2027年：数億人が使用するサービスを開発、運営。多くの人々を幸せにする。
+            </p>
+            <p className="px-4 pt-6 pb-1 text-lg font-semibold leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              目標
+            </p>
+            <p className="px-4 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              大規模なアプリケーションの開発に携わり、多くの人々の幸せを支えたい。
+            </p>
+            <p className="px-4 pt-6 pb-1 text-lg font-semibold leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+              概要
+            </p>
+            <p className="px-4 pb-4 text-lg leading-8 tracking-normal text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-9 md:text-xl md:leading-10">
+            このWebサイトでは「開発したプロダクト」、「情報を共有するブログ」を掲載します。
             </p>
           </div>
           <div className="mt-16 flex flex-col border-2 border-gray-500 rounded border-opacity-60">
             <h2 className="px-4 py-4 text-xl font-bold tracking-normal text-gray-900 dark:text-gray-100 md:text-2xl">
-              プログラミング技術
+               スキルセット
             </h2>
             <div className="px-2 py-4 flex flex-wrap">
               <div className="text-xs text-gray-100 dark:text-gray-900 text-center bg-gray-900 dark:bg-gray-100 px-4 py-2 m-2 rounded-md">
@@ -73,6 +121,12 @@ export default function Profile() {
               </div>
               <div className="text-xs text-gray-100 dark:text-gray-900 text-center bg-gray-900 dark:bg-gray-100 px-4 py-2 m-2 rounded-md">
                 Next.js
+              </div>
+              <div className="text-xs text-gray-100 dark:text-gray-900 text-center bg-gray-900 dark:bg-gray-100 px-4 py-2 m-2 rounded-md">
+                Svelte
+              </div>
+              <div className="text-xs text-gray-100 dark:text-gray-900 text-center bg-gray-900 dark:bg-gray-100 px-4 py-2 m-2 rounded-md">
+                Nest.js
               </div>
               <div className="text-xs text-gray-100 dark:text-gray-900 text-center bg-gray-900 dark:bg-gray-100 px-4 py-2 m-2 rounded-md">
                 Ruby on Rails
